864c3f6bc340ea8ca73a5a99d9b5a02f40e8fbc9
AliDzaki <alidzakifikri@gmail.com>
2024-02-10 12:10:38 +0700

    menambahkan kata ke-2

--- 2023-02-10.php
+++ 2023-02-10.php
@@ -10,3 +10,15 @@ function move($pos, $roll) {
     return $pos + ($roll * 2);
   }
 move(0,9);
+
+// kata 2
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+// solusi:
+function twoSort($s) {
+    sort($s);
+    return implode("***",str_split($s[0]));
+  }
