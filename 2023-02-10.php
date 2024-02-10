8e67467d894dc8cda176a57f7b32e6e59b92088c
AliDzaki <alidzakifikri@gmail.com>
2024-02-10 12:35:35 +0700

    Menambahkan kata ke-4

--- 2023-02-10.php
+++ 2023-02-10.php
@@ -46,3 +46,36 @@ function square_digits($num): int {
     return $result;
   }
 
+// kata 4
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here:
+// solusi:
+function DNA_strand($dna) {
+    $arr = str_split($dna);
+    $result ="";
+    foreach($arr as $string){
+      switch($string){
+          case "A":
+          $result .= "T";
+          break;
+          case "T":
+          $result .= "A";
+          break;
+          case "C":
+          $result .= "G";
+          break;
+          case "G":
+          $result .= "C";
+          break;
+          default:
+          $result .= "";
+      }
+      
+    }
+    return $result;
+  }
