0d49da47fbcae1389d7bf4fad3dde36da8a199b6
AliDzaki <alidzakifikri@gmail.com>
2024-02-12 20:06:52 +0700

    menambahkan kata baru

--- 2024-02-12.php
+++ 2024-02-12.php
@@ -1,2 +1,16 @@
 <?php
-//
+// day 2
+//kata 1
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// solusi :
+function even_or_odd(int $n): string {
+    return $n % 2 == 0 ? "Even" : "Odd";
+   }
+
+function even_or_odd2($n){
+    if($n % 2 == 0){
+        return "Even";
+    }else{
+        return "Odd";
+    }
+}
