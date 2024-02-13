bd8f07b02f9c4490ea6b02eb4a8a5d55da3f6aac
AliDzaki <alidzakifikri@gmail.com>
2024-02-13 22:07:58 +0700

    Menambahkan kata baru di file baru

--- 2024-02-13.php
+++ 2024-02-13.php
@@ -1,1 +1,19 @@
 <?php
+// kata 1
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// solusi : 
+function series_sum($n)
+{
+    $pembagi = 1;
+    $sum = 0.00;
+    $angka = 1;
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i === 1) {
+            $sum = 1;
+        } else {
+            $pembagi += 3;
+            $sum += 1 / $pembagi;
+        }
+    }
+    return strval(number_format($sum, 2, '.', ''));
+}
