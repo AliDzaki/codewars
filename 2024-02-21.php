f728d235e7ffc9af5d3e6059c8392340d6f92ee9
AliDzaki <alidzakifikri@gmail.com>
2024-02-21 20:15:42 +0700

    Menambahkan kata untuk tantangan pertama

--- 2024-02-21.php
+++ 2024-02-21.php
@@ -1,1 +1,11 @@
 <?php
+// kata 1
+// Array.diff
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// solusi :
+function arrayDiff($a, $b) {
+  
+    $result = array_diff($a, $b);
+     return array_values($result);
+   
+   }
